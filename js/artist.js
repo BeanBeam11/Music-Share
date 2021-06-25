6e1831bfd1c13064314c242e8dbe0cbc84a6b382
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-06-25 16:44:11 +0800

    edit artist round gsap

--- js/artist.js
+++ js/artist.js
@@ -1,4 +1,37 @@
-// gsap.to('.round1', {
-//     duration: 1,
-//     x: 500,
-// });
+init();
+
+function init(){
+    gsap.to('.round1', {
+        duration: 0,
+        x: 500,
+    });
+    gsap.to('.round2', {
+        duration: 0,
+        x: 300,
+    });
+    gsap.to('.round4', {
+        duration: 0,
+        x: -300,
+    });
+    gsap.to('.round5', {
+        duration: 0,
+        x: -500,
+    });
+}
+
+gsap.to('.round1', {
+    duration: 1,
+    x: 0,
+});
+gsap.to('.round2', {
+    duration: 1,
+    x: 0,
+});
+gsap.to('.round4', {
+    duration: 1,
+    x: 0,
+});
+gsap.to('.round5', {
+    duration: 1,
+    x: 0,
+});
