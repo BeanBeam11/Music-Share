7576f3ec71fbaa3605186b22f1389a78b34f8e0f
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-06-26 21:53:48 +0800

    add artist scrollTrigger

--- js/artist.js
+++ js/artist.js
@@ -1,3 +1,5 @@
+gsap.registerPlugin(ScrollTrigger);
+
 init();
 
 function init(){
@@ -17,6 +19,30 @@ function init(){
         duration: 0,
         x: -500,
     });
+    gsap.to('.intro1 .intro-round', {
+        duration: 0,
+        x: -500,
+    });
+    gsap.to('.intro2 .intro-round', {
+        duration: 0,
+        x: 500,
+    });
+    gsap.to('.intro3 .intro-round', {
+        duration: 0,
+        x: -500,
+    });
+    gsap.to('.intro1 .intro-info', {
+        duration: 0,
+        opacity: 0
+    });
+    gsap.to('.intro2 .intro-info', {
+        duration: 0,
+        opacity: 0
+    });
+    gsap.to('.intro3 .intro-info', {
+        duration: 0,
+        opacity: 0
+    });
 }
 
 gsap.to('.round1', {
@@ -34,4 +60,35 @@ gsap.to('.round4', {
 gsap.to('.round5', {
     duration: 1,
     x: 0,
+});
+/* scrollTrigger */
+gsap.to('.intro1 .intro-round', {
+    duration: 1,
+    x: 0,
+    scrollTrigger: '.intro1 .intro-round'
+});
+gsap.to('.intro2 .intro-round', {
+    duration: 1,
+    x: 0,
+    scrollTrigger: '.intro2 .intro-round'
+});
+gsap.to('.intro3 .intro-round', {
+    duration: 1,
+    x: 0,
+    scrollTrigger: '.intro3 .intro-round'
+});
+gsap.to('.intro1 .intro-info', {
+    duration: 2,
+    opacity: 1,
+    scrollTrigger: '.intro1 .intro-round'
+});
+gsap.to('.intro2 .intro-info', {
+    duration: 2,
+    opacity: 1,
+    scrollTrigger: '.intro2 .intro-round'
+});
+gsap.to('.intro3 .intro-info', {
+    duration: 2,
+    opacity: 1,
+    scrollTrigger: '.intro3 .intro-round'
 });
