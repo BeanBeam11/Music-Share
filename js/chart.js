822d75be7d57a2ad0fa906053f38d415255bbc46
BeanBeam <s110819015@stu.ntue.edu.tw>
2021-06-26 21:22:15 +0800

    add chart scroll

--- js/chart.js
+++ js/chart.js
@@ -47,26 +47,38 @@ function setNum(sData){
     let num_olivia = 0;
     let num_taylor = 0;
     let num_dua = 0;
+    let num_shawn = 0;
+    let num_charlie = 0;
 
     for(let i=1; i<sData.length; i++){
-        if(sData[i][1] == 'Taylor Swift'){
-            num_taylor++;
-        }
         if(sData[i][1] == 'Olivia Rodrigo'){
             num_olivia++;
         }
+        if(sData[i][1] == 'Taylor Swift'){
+            num_taylor++;
+        }
         if(sData[i][1] == 'Dua Lipa'){
             num_dua++;
+        }
+        if(sData[i][1] == 'Shawn Mendes'){
+            num_shawn++;
+        }
+        if(sData[i][1] == 'Charlie Puth'){
+            num_charlie++;
         }
     }
 
     console.log(num_taylor);
     console.log(num_olivia);
     console.log(num_dua);
+    console.log(num_shawn);
+    console.log(num_charlie);
 
     $('#olivia .vote-num').html(num_olivia + ' votes');
     $('#taylor .vote-num').html(num_taylor + ' votes');
     $('#dua .vote-num').html(num_dua + ' votes');
+    $('#shawn .vote-num').html(num_shawn + ' votes');
+    $('#charlie .vote-num').html(num_charlie + ' votes');
 }
 
 function refreshPage(){
